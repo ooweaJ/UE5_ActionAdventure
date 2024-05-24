73a4349d66283ed90feb7a34bf38758a11652886
ooweaJ <dbwidn2@naver.com>
2024-05-24 17:49:26 +0900

    GameMode

--- Source/ActionAdventure/Core/GameMode/MainWorldGameMode.cpp
+++ Source/ActionAdventure/Core/GameMode/MainWorldGameMode.cpp
@@ -1,5 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Core/GameMode/MainWorldGameMode.h"
+#include "Characters/Players/CPlayer.h"
+#include "Characters/Controller/CPlayerController.h"
 
+AMainWorldGameMode::AMainWorldGameMode()
+{
+	{
+		ConstructorHelpers::FClassFinder<ACPlayer> Class(TEXT("/Script/Engine.Blueprint'/Game/_dev/Characters/Players/BP_Player.BP_Player_C'"));
+		if (Class.Succeeded())
+			PawnClass = Class.Class;
+	}
+
+	{
+		ConstructorHelpers::FClassFinder<ACPlayerController> Class(TEXT("/Script/Engine.Blueprint'/Game/_dev/Characters/Players/BP_CPlayerController.BP_CPlayerController_C'"));
+		if (Class.Succeeded())
+			PlayConClass = Class.Class;
+	}
+
+	DefaultPawnClass = PawnClass;
+	PlayerControllerClass = PlayConClass;
+}
--- Source/ActionAdventure/Core/GameMode/MainWorldGameMode.h
+++ Source/ActionAdventure/Core/GameMode/MainWorldGameMode.h
@@ -1,17 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "MainWorldGameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ACTIONADVENTURE_API AMainWorldGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	AMainWorldGameMode();
+
+	FORCEINLINE bool IsMaxAI() { return MaxAI == CurrentAI; }
+private:
+	TSubclassOf<class ACPlayer> PawnClass;
+	TSubclassOf<class ACPlayerController> PlayConClass;
+
+	int32 MaxAI = 20;
+	int32 CurrentAI = 0;
 };
